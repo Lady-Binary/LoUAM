e75be19c213c965ab4b97d89524fc656ef12fd73
Lady-Binary <ladybinary@protonmail.com>
2021-01-26 14:13:57 +0000

    remove imports as well

--- LoUAM/MapGenerator.xaml.cs
+++ LoUAM/MapGenerator.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Win32;
-using Ookii.Dialogs.Wpf;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
 using System.Windows;
 using System.ComponentModel;
